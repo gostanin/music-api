27d0a6672218cfaafb81838b96b1e30943d4242a
Gregory Ostanin <ostingreg@gmail.com>
2020-09-01 15:10:08 -0700

    binary fix

--- music/utils.py
+++ music/utils.py
@@ -1,4 +1,4 @@
 def read_binary_stream(start_byte, end_byte, path):
-    result = f'song right here {path}  {0*start_byte} - {1*end_byte}'
+    result = f'song right here {path}  {"0"*start_byte} - {"1"*end_byte}'
 
-    return bin(result)
+    return result

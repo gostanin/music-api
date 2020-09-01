b78d746f68f22b0a7f2b05238e1a758ecd87cfd5
Gregory Ostanin <ostingreg@gmail.com>
2020-09-01 15:28:58 -0700

    settings update

--- music/settings.py
+++ music/settings.py
@@ -9,6 +9,4 @@ ROOT_DIR = Path(__file__).parent.parent.absolute()
 DEBUG = config('DEBUG', default=True, cast=bool)
 PORT = config('PORT', default=8000, cast=int)
 
-APPLICATION_ROOT = '/api/v1'
-
 ALLOWED_CORS_ORIGINS = config('ALLOWED_CORS_ORIGINS', default='*')
